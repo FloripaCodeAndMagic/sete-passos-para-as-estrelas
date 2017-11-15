0c061353fcfa7518039a955a911777f98306e127
Lucas Vieira <vieiralucas4@gmail.com>
2017-11-15 20:42:09 -0200

    Add FetchPage

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"net/url"
 )
 
 func main() {
-
 }
 
 func buildQuery(pageName string) string {
@@ -18,4 +20,69 @@ func buildQuery(pageName string) string {
 	v.Add("format", "json")
 
 	return fmt.Sprintf("/w/api.php?%s", v.Encode())
+}
+
+type Link struct {
+	Title string `json:"title"`
+}
+
+type Page struct {
+	Title string `json:"title"`
+	Links []Link `json:"links"`
+}
+
+func (p *Page) articles() []string {
+	articles := []string{}
+
+	for _, l := range p.Links {
+		articles = append(articles, l.Title)
+	}
+
+	return articles
+}
+
+type WikiQuery struct {
+	Pages map[string]Page `json:"pages"`
+}
+
+type WikiResponse struct {
+	Query WikiQuery `json:"query"`
+}
+
+func (wq *WikiQuery) articles() []string {
+	articles := []string{}
+	for _, page := range wq.Pages {
+		articles = append(articles, page.articles()...)
+	}
+
+	return articles
+}
+
+func (wr *WikiResponse) articles() []string {
+	return wr.Query.articles()
+}
+
+func fetchPage(wikiUrl string, pageName string) ([]string, error) {
+	query := buildQuery(pageName)
+	url := fmt.Sprintf("%s%s", wikiUrl, query)
+	res, err := http.Get(url)
+	defer res.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	wikiResponse := WikiResponse{}
+	err = json.Unmarshal(body, &wikiResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	articles := wikiResponse.articles()
+
+	return articles, nil
 }
--- main_test.go
+++ main_test.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"net/url"
+	"reflect"
 	"testing"
 )
 
@@ -20,5 +24,91 @@ func TestBuildQuery(t *testing.T) {
 
 	if query != expected {
 		t.Errorf("Expected %s got %s", expected, query)
+	}
+}
+
+func newTestWikiResponse(titles []string) WikiResponse {
+	links := []Link{}
+	for _, l := range titles {
+		links = append(links, Link{l})
+	}
+
+	wr := WikiResponse{
+		Query: WikiQuery{
+			Pages: map[string]Page{
+				"123": Page{
+					Title: "Joseph Opala",
+					Links: links,
+				},
+			},
+		},
+	}
+
+	return wr
+}
+
+func TestFetchPage(t *testing.T) {
+	testHandler := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		payload := newTestWikiResponse([]string{"whatever"})
+
+		jsonPayload, err := json.Marshal(payload)
+		if err != nil {
+			t.Errorf("Failed to encode mocked data")
+		}
+		w.Write(jsonPayload)
+
+		wikiResponse := WikiResponse{}
+		err = json.Unmarshal(jsonPayload, &wikiResponse)
+
+		if err != nil {
+			t.Errorf("Failed to encode mocked data")
+		}
+
+	}
+	ts := httptest.NewServer(http.HandlerFunc(testHandler))
+	defer ts.Close()
+
+	articles, err := fetchPage(ts.URL, "Joseph Opala")
+	if err != nil {
+		t.Errorf("Unexpected fetch error")
+	}
+
+	expected := []string{"whatever"}
+	if !reflect.DeepEqual(articles, expected) {
+		t.Errorf("Expected %v, got %v", expected, articles)
+	}
+}
+
+func TestFetchPageNoTitles(t *testing.T) {
+	testHandler := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		payload := newTestWikiResponse([]string{})
+
+		jsonPayload, err := json.Marshal(payload)
+		if err != nil {
+			t.Errorf("Failed to encode mocked data")
+		}
+		w.Write(jsonPayload)
+
+		wikiResponse := WikiResponse{}
+		err = json.Unmarshal(jsonPayload, &wikiResponse)
+
+		if err != nil {
+			t.Errorf("Failed to encode mocked data")
+		}
+
+	}
+	ts := httptest.NewServer(http.HandlerFunc(testHandler))
+	defer ts.Close()
+
+	articles, err := fetchPage(ts.URL, "Joseph Opala")
+	if err != nil {
+		t.Errorf("Unexpected fetch error")
+	}
+
+	expected := []string{}
+	if !reflect.DeepEqual(articles, expected) {
+		t.Errorf("Expected %v, got %v", expected, articles)
 	}
 }
